ba62cb8b5cd09e5ad5557a5134a74cc2a7899373
oguzhanaydiin <Oguuzaydinn@gmail.com>
2026-07-03 00:57:53 +0200

    adding packages

--- neydi-be/app/core/config.py
+++ neydi-be/app/core/config.py
@@ -8,6 +8,16 @@ class Settings(BaseSettings):
 
     ALLOWED_ORIGINS: list[str] = ["http://localhost:3000"]
 
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
     class Config:
         env_file = ".env"
 
