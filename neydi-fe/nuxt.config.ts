c7332d8319047374b0b8214a1ec6e8df9044bcae
oguzhanaydiin <Oguuzaydinn@gmail.com>
2026-07-04 00:37:31 +0200

    adding auth functions

--- neydi-fe/nuxt.config.ts
+++ neydi-fe/nuxt.config.ts
@@ -16,6 +16,12 @@ export default defineNuxtConfig({
     fallback: 'dark'
   },
 
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://127.0.0.1:8000'
+    }
+  },
+
   compatibilityDate: '2025-01-15',
 
   eslint: {
